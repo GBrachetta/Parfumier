790002aeaa6105fd25781a17040d57eaf2071592
Guillermo Brachetta <brachetta@me.com>
2020-05-02 17:03:53 +0200

    Created basic/empty routes

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,18 @@ from app import app, mongo
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
+
+
+@app.route('/login ')
+def login():
+    return render_template('login.html', title='Login')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    return render_template('register.html', title='Register')
