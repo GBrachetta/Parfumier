082be7b8cb8643ca4c0bf0e0b8bd0f670cf54096
Guillermo Brachetta <brachetta@me.com>
2020-05-03 03:26:14 +0200

    Added update account form

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from wtforms.validators import DataRequired, EqualTo, ValidationError, Email, Le
 from flask_wtf import FlaskForm
 from wtforms import(StringField, PasswordField, SubmitField, TextAreaField,
                     SelectMultipleField, FieldList, widgets, BooleanField)
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -34,3 +35,24 @@ class LoginForm(FlaskForm):
                              DataRequired(), Length(min=6)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = mongo.db.users.find_one({'username': username.data})
+            if user:
+                raise ValidationError('The username already exists.')
+
+    def validate_email(self, email):
+        if username.data != current_user.username:
+            user = mongo.db.users.find_one({'email': email.data})
+            if user:
+                raise ValidationError('The email already exists.')
