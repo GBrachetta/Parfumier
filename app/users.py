acc84236649ea0f807bf92611955e252e85a0e77
Guillermo Brachetta <brachetta@me.com>
2020-05-05 12:05:43 +0200

    Started work on the method to request pass reset email

--- app/users.py
+++ app/users.py
@@ -1,5 +1,11 @@
 from werkzeug.security import check_password_hash
-from app import login_manager, mongo
+from app import login_manager, mongo, app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import logging  # Delete this for prod
+
+# LOGGING
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w',
+                    format='%(asctime)s %(name)s - %(filename)s :: %(lineno)d - %(levelname)s - %(message)s\n', datefmt='%Y-%m-%d %H:%M:%S')
 
 
 class User():
@@ -27,6 +33,44 @@ class User():
     @staticmethod
     def check_password(password_hash, password):
         return check_password_hash(password_hash, password)
+
+
+# START ATTEMPT TO SEND RESET PASSWORD EMAIL #
+
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY', expires_sec])
+        return s.dumps({'_id': self._id})
+
+# COREY
+    # def get_reset_token(self, expires_sec=1800):
+    #     s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+    #     return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            _id = s.loads(token)['_id']
+            logging.debug(_id) # LOGGING
+        except:
+            return None
+        logging.debug(_id) # LOGGING
+        return User.find_one({'_id': _id})
+
+# COREY
+    # @staticmethod
+    # def verify_reset_token(token):
+    #     s = Serializer(current_app.config['SECRET_KEY'])
+    #     try:
+    #         user_id = s.loads(token)['user_id']
+    #     except:
+    #         return None
+    #     return User.query.get(user_id)
+    # def __repr__(self):
+    #     return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+# END ATTEMPT TO SEND RESET PASSWORD EMAIL #
 
 
 @login_manager.user_loader
