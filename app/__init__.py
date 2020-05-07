9b05c0d7474f51a7e3b0ec093c992c1b0133e781
Guillermo Brachetta <brachetta@me.com>
2020-05-08 00:02:02 +0200

    Changed formatter to black - Formatted

--- app/__init__.py
+++ app/__init__.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message = 'Please log in.'
-login_manager.login_message_category = 'info'
-app.config['MONGO_URI'] = Config.MONGO_URI
+login_manager.login_view = "login"
+login_manager.login_message = "Please log in."
+login_manager.login_message_category = "info"
+app.config["MONGO_URI"] = Config.MONGO_URI
 mongo = PyMongo(app)
-app.config['MAIL_SERVER'] = 'smtp.strato.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USER")
-app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASS")
+app.config["MAIL_SERVER"] = "smtp.strato.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASS")
 mail = Mail(app)
 
 from app import routes, models
