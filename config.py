600e65449ea447e421f381f5c2aa5b475dc3609f
Guillermo Brachetta <brachetta@me.com>
2020-05-08 00:00:31 +0200

    Changed formatter to black - Formatted

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config(object):
-    '''
+    """
     EXPLANATION
-    '''
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MONGO_URI = os.environ.get('MONGO_URI')
+    """
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    MONGO_URI = os.environ.get("MONGO_URI")
     DEBUG = True
-    
