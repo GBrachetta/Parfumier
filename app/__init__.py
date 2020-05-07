516d7ac4f8935bac25c13396ea4baa728838da31
Guillermo Brachetta <brachetta@me.com>
2020-05-08 00:59:57 +0200

    Deleted unused imports and configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
-import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_pymongo import PyMongo
-from flask_mail import Mail
 from config import Config
 
 app = Flask(__name__)
@@ -14,11 +12,6 @@ login_manager.login_message = "Please log in."
 login_manager.login_message_category = "info"
 app.config["MONGO_URI"] = Config.MONGO_URI
 mongo = PyMongo(app)
-app.config["MAIL_SERVER"] = "smtp.strato.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USER")
-app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASS")
-mail = Mail(app)
+
 
 from app import routes, models
