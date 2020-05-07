cbed2bfa32e45f5eb59d11bdef7b7be5d40ebeae
Guillermo Brachetta <brachetta@me.com>
2020-05-08 00:02:26 +0200

    Changed formatter to black - Formatted

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,11 @@
 import logging
-from wtforms.validators import DataRequired, EqualTo, ValidationError, Email, Length
+from wtforms.validators import (
+    DataRequired,
+    EqualTo,
+    ValidationError,
+    Email,
+    Length,
+)
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
@@ -7,102 +13,126 @@ from flask_login import current_user
 from app import mongo
 
 # LOGGING
-logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w',
-                    format='%(asctime)s %(name)s - %(filename)s :: %(lineno)d - %(levelname)s - %(message)s\n', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="app.log",
+    filemode="w",
+    format="%(asctime)s %(name)s - %(filename)s :: %(lineno)d - %(levelname)s - %(message)s\n",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 class RegistrationForm(FlaskForm):
-    '''
+    """
     DESCRIPTION
-    '''
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
-                                                                     EqualTo('password')])
-    submit = SubmitField('Register')
+    """
+
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=6)]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Register")
 
     def validate_username(self, username):
-        '''
+        """
         DESCRIPTION
-        '''
-        user = mongo.db.users.find_one({'username': username.data})
+        """
+        user = mongo.db.users.find_one({"username": username.data})
         if user:
-            raise ValidationError('The username already exists.')
+            raise ValidationError("The username already exists.")
 
     def validate_email(self, email):
-        '''
+        """
         DESCRIPTION
-        '''
-        user = mongo.db.users.find_one({'email': email.data})
+        """
+        user = mongo.db.users.find_one({"email": email.data})
         if user:
-            raise ValidationError('The email already exists.')
+            raise ValidationError("The email already exists.")
 
 
 class LoginForm(FlaskForm):
-    '''
+    """
     DESCRIPTION
-    '''
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    """
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=6)]
+    )
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
 
 
 class UpdateAccountForm(FlaskForm):
-    '''
+    """
     DESCRIPTION
-    '''
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    avatar = FileField('Upload your avatar', validators=[FileAllowed(['jpg', 'png'])])
+    """
 
-    submit = SubmitField('Update')
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    avatar = FileField(
+        "Upload your avatar", validators=[FileAllowed(["jpg", "png"])]
+    )
+
+    submit = SubmitField("Update")
 
     def validate_username(self, username):
-        '''
+        """
         DESCRIPTION
-        '''
+        """
         if username.data != current_user.username:
-            user = mongo.db.users.find_one({'username': username.data})
+            user = mongo.db.users.find_one({"username": username.data})
             if user:
-                raise ValidationError('The username already exists.')
+                raise ValidationError("The username already exists.")
 
     def validate_email(self, email):
-        '''
+        """
         DESCRIPTION
-        '''
+        """
         if email.data != current_user.email:
-            user = mongo.db.users.find_one({'email': email.data})
+            user = mongo.db.users.find_one({"email": email.data})
             if user:
-                raise ValidationError('The email already exists.')
+                raise ValidationError("The email already exists.")
 
 
 class RequestResetForm(FlaskForm):
-    '''
+    """
     DESCRIPTION
-    '''
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
+    """
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
 
     def validate_email(self, email):
-        '''
+        """
         DESCRIPTION
-        '''
-        user = mongo.db.users.find_one({'email': email.data})
+        """
+        user = mongo.db.users.find_one({"email": email.data})
         if user is None:
-            raise ValidationError('There is no accout with that email.')
+            raise ValidationError("There is no accout with that email.")
 
 
 class ResetPasswordForm(FlaskForm):
-    '''
+    """
     DESCRIPTION
-    '''
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
-                                                                     EqualTo('password')])
-    submit = SubmitField('Reset Password')
+    """
+
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=6)]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Reset Password")
