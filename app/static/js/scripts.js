9f22d7e950885d2ebd82861596b77559c7b22f8b
Guillermo Brachetta <brachetta@me.com>
2020-05-10 19:18:00 +0200

    Attempted to reproduce function to work on new preview

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -17,3 +17,21 @@ EL_avatar.addEventListener("change", function (ev) {
         FR.readAsDataURL(file);
     }
 });
+
+const EL_picture = document.querySelector("#picture");
+const EL_pictureFileName = document.querySelector("#pictureFileName");
+const EL_previewPicture = document.querySelector("#thumb-picture");
+
+EL_picture.addEventListener("change", function (ev) {
+    const files = ev.target.files;
+
+    if (files && files[0]) {
+        const file = files[0];
+        EL_pictureFileName.textContent = `Picture selected`;
+        const FR = new FileReader();
+        FR.addEventListener("load", function (ev) {
+            EL_previewPicture.style.backgroundImage = `url("${ev.target.result}")`;
+        });
+        FR.readAsDataURL(file);
+    }
+});
