f85b69952a6a6d40a1f04059abc4c430c9587b8c
Guillermo Brachetta <brachetta@me.com>
2020-05-10 19:16:50 +0200

    Added function to save perfume picture reduced in size

--- app/utils.py
+++ app/utils.py
@@ -22,8 +22,32 @@ def save_avatar(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, "static/images/avatars", picture_fn)
+    picture_path = os.path.join(
+        app.root_path, "static/images/avatars", picture_fn
+    )
     output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+def save_picture(form_picture):
+    """Saves picture to file system
+
+    Keyword arguments:
+    argument -- description
+    Return: returns a file to the file system with dimensions reduced
+            as defined in the output size.
+    """
+
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(
+        app.root_path, "static/images/perfume_pics", picture_fn
+    )
+    output_size = (300, 300)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
