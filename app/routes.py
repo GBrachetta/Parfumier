507913493d223ede34e0ff2f2aeb4fa30790d13f
Guillermo Brachetta <brachetta@me.com>
2020-05-10 12:25:13 +0200

    Moved method send_reset_email to utils.py file

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,5 @@
 import os
 import logging
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import ssl
-import smtplib
 from flask import render_template, redirect, flash, url_for, request
 from werkzeug.security import generate_password_hash
 from flask_login import login_user, logout_user, current_user, login_required
@@ -16,7 +12,7 @@ from app.forms import (
     RequestResetForm,
     ResetPasswordForm,
 )
-from app.utils import save_avatar
+from app.utils import save_avatar, send_reset_email
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -179,64 +175,6 @@ def logout():
     logout_user()
     flash("We hope to see you back soon again!", "warning")
     return redirect(url_for("index"))
-
-
-def send_reset_email(user):
-    """
-    DESCRIPTION
-    """
-    reset_user = User(
-        username=user["username"],
-        first_name=user["first_name"],
-        last_name=user["last_name"],
-        email=user["email"],
-        _id=user["_id"],
-        is_admin=user["is_admin"],
-        avatar=user["avatar"],
-    )
-    token = reset_user.get_reset_token()
-    sender_email = os.environ.get("MAIL_USERNAME")
-    password_email = os.environ.get("MAIL_PASSWORD")
-    receiver_email = user["email"]
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "Password Reset Request"
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    receiver = mongo.db.users.find_one({"email": receiver_email})
-    text = f"""You have requested to reset your password for your account on Parfumier.
-
-To reset your password, please visit the following link:
-
-{url_for('reset_token', token=token, _external=True)}
-
-If you did not make this request then simply ignore this email and no changes will be made.
-
-Best regards,
-
-Parfumier
-"""
-    html = f"""
-    <html>
-    <body>
-    <h3>Dear <strong>{receiver['username']}</strong>,</h3><br>
-       <p>You have requested to reset your password for your account on Parfumier.<br>
-       <p>To reset your password, please visit the following link:<br><br>
-       <a href="{url_for('reset_token', token=token, _external=True)}">Reset Password</a><br><br>
-       If you did not make this request then simply ignore this email and no changes will be made.<br><br>
-       Best Regards,<br>
-       <em>Parfumier</em>
-    </p>
-    </body>
-    </html>"""
-
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
-    message.attach(part1)
-    message.attach(part2)
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.strato.com", 465, context=context) as server:
-        server.login(sender_email, password_email)
-        server.sendmail(sender_email, receiver_email, message.as_string())
 
 
 @app.route("/reset_password", methods=["GET", "POST"])
--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,13 @@
 import os
 import secrets
-from app import app
+from app import app, mongo
 from PIL import Image
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import ssl
+import smtplib
+from app.models import User
+from flask import url_for
 
 
 def save_avatar(form_picture):
@@ -17,3 +23,61 @@ def save_avatar(form_picture):
     i.thumbnail(output_size)
     i.save(picture_path)
     return picture_fn
+
+
+def send_reset_email(user):
+    """
+    DESCRIPTION
+    """
+    reset_user = User(
+        username=user["username"],
+        first_name=user["first_name"],
+        last_name=user["last_name"],
+        email=user["email"],
+        _id=user["_id"],
+        is_admin=user["is_admin"],
+        avatar=user["avatar"],
+    )
+    token = reset_user.get_reset_token()
+    sender_email = os.environ.get("MAIL_USERNAME")
+    password_email = os.environ.get("MAIL_PASSWORD")
+    receiver_email = user["email"]
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Password Reset Request"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    receiver = mongo.db.users.find_one({"email": receiver_email})
+    text = f"""You have requested to reset your password for your account on Parfumier.
+
+To reset your password, please visit the following link:
+
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+
+Best regards,
+
+Parfumier
+"""
+    html = f"""
+    <html>
+    <body>
+    <h3>Dear <strong>{receiver['username']}</strong>,</h3><br>
+       <p>You have requested to reset your password for your account on Parfumier.<br>
+       <p>To reset your password, please visit the following link:<br><br>
+       <a href="{url_for('reset_token', token=token, _external=True)}">Reset Password</a><br><br>
+       If you did not make this request then simply ignore this email and no changes will be made.<br><br>
+       Best Regards,<br>
+       <em>Parfumier</em>
+    </p>
+    </body>
+    </html>"""
+
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    message.attach(part1)
+    message.attach(part2)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.strato.com", 465, context=context) as server:
+        server.login(sender_email, password_email)
+        server.sendmail(sender_email, receiver_email, message.as_string())
