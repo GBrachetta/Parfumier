f6e44582968fbcec5d3b86cdb809937b1913e51a
Guillermo Brachetta <brachetta@me.com>
2020-05-21 23:08:08 +0200

    Registered blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 login_manager = LoginManager(app)
-login_manager.login_view = "login"
+login_manager.login_view = "usersBP.login"
 login_manager.login_message = "Please log in."
 login_manager.login_message_category = "info"
 app.config["MONGO_URI"] = Config.MONGO_URI
@@ -17,4 +17,17 @@ mongo = PyMongo(app)
 Imports bellow need to happen after initialisation of the app
 to prevent circular imports.
 """
-from app import routes, models
+
+from app import models
+
+from app.usersBP.routes import usersBP
+from app.perfumesBP.routes import perfumesBP
+from app.typesBP.routes import typesBP
+from app.reviewsBP.routes import reviewsBP
+from app.mainBP.routes import mainBP
+
+app.register_blueprint(usersBP)
+app.register_blueprint(perfumesBP)
+app.register_blueprint(typesBP)
+app.register_blueprint(reviewsBP)
+app.register_blueprint(mainBP)
