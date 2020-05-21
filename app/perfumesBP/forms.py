1639512884444d2223fa59d9f514be5561a257c6
Guillermo Brachetta <brachetta@me.com>
2020-05-21 23:04:11 +0200

    Created Blueprint for perfumes - Moved all perfume routes and forms to blueprint

---
+++ app/perfumesBP/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    TextAreaField,
+    BooleanField,
+    SubmitField,
+    SelectField,
+)
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed
+
+
+class CreatePerfumeForm(FlaskForm):
+    brand = StringField("Brand", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    description = TextAreaField("Description")
+    public = BooleanField("Public")
+    submit = SubmitField("Create")
+    picture = FileField("Picture", validators=[FileAllowed(["jpg", "png"])])
+    perfume_type = StringField("Type", validators=[DataRequired()])
+
+
+class EditPerfumeForm(FlaskForm):
+    brand = StringField("Brand", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    description = TextAreaField("Description")
+    public = BooleanField("Public")
+    submit = SubmitField("Update")
+    picture = FileField("Picture", validators=[FileAllowed(["jpg", "png"])])
+    perfume_type = StringField("Type", validators=[DataRequired()])
+
+
+class SearchForm(FlaskForm):
+    choices = [("Brand", "Brand"), ("Perfume", "Perfume"), ("Type", "Type")]
+    select = SelectField("Search:", choices=choices)
+    search = StringField("")
