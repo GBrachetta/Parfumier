881d0c31dd94e65258c493cff5b7614c89729a93
Guillermo Brachetta <brachetta@me.com>
2020-05-22 01:34:29 +0200

    Moved creation of the app to its own function

--- app/__init__.py
+++ app/__init__.py
@@ -3,28 +3,34 @@ from flask_login import LoginManager
 from flask_pymongo import PyMongo
 from app.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-login_manager = LoginManager(app)
+
+login_manager = LoginManager()
 login_manager.login_view = "users.login"
 login_manager.login_message = "Please log in."
 login_manager.login_message_category = "info"
-mongo = PyMongo(app)
+mongo = PyMongo()
 
 """
 Imports bellow need to happen after initialisation of the app
 to prevent circular imports.
 """
 
-from app import models
-from app.users.routes import users
-from app.perfumes.routes import perfumes
-from app.types.routes import types
-from app.reviews.routes import reviews
-from app.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(perfumes)
-app.register_blueprint(types)
-app.register_blueprint(reviews)
-app.register_blueprint(main)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    login_manager.init_app(app)
+    mongo.init_app(app)
+    from app.users.routes import users
+    from app.perfumes.routes import perfumes
+    from app.types.routes import types
+    from app.reviews.routes import reviews
+    from app.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(perfumes)
+    app.register_blueprint(types)
+    app.register_blueprint(reviews)
+    app.register_blueprint(main)
+
+    return app
