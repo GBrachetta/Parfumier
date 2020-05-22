fbdaec26be23c7c35a46650cd17301656fc526f5
Guillermo Brachetta <brachetta@me.com>
2020-05-22 02:23:04 +0200

    Reverting to resucitate app crashing

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from app import create_app
+from app import app
 
-app = create_app()
 
 if __name__ == "__main__":
     app.run()
--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,35 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_pymongo import PyMongo
-from app.config import Config
+from config import Config
 
+app = Flask(__name__)
 
-login_manager = LoginManager()
+app.config.from_object(Config)
+login_manager = LoginManager(app)
 login_manager.login_view = "users.login"
 login_manager.login_message = "Please log in."
 login_manager.login_message_category = "info"
-mongo = PyMongo()
+app.config["MONGO_URI"] = Config.MONGO_URI
+mongo = PyMongo(app)
 
 """
 Imports bellow need to happen after initialisation of the app
 to prevent circular imports.
 """
 
+from app import models
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    login_manager.init_app(app)
-    mongo.init_app(app)
-    from app.users.routes import users
-    from app.perfumes.routes import perfumes
-    from app.types.routes import types
-    from app.reviews.routes import reviews
-    from app.main.routes import main
-    from app.errors.handlers import errors
+from app.users.routes import users
+from app.perfumes.routes import perfumes
+from app.types.routes import types
+from app.reviews.routes import reviews
+from app.main.routes import main
+from app.errors.handlers import errors
 
-    app.register_blueprint(users)
-    app.register_blueprint(perfumes)
-    app.register_blueprint(types)
-    app.register_blueprint(reviews)
-    app.register_blueprint(main)
-    app.register_blueprint(errors)
-
-    return app
+app.register_blueprint(users)
+app.register_blueprint(perfumes)
+app.register_blueprint(types)
+app.register_blueprint(reviews)
+app.register_blueprint(main)
+app.register_blueprint(errors)
