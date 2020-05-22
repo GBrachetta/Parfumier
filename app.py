00d04097f6c5d29607f1c1f690c585febfc01b94
Guillermo Brachetta <brachetta@me.com>
2020-05-22 13:00:21 +0200

    New reverting back to functional state trying to troubleshoot issue - Will create separate branch

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # Working
-# from app import app
+from app import app
 
-
-# if __name__ == "__main__":
-#     app.run()
-
-
-# Want it working
-from app import create_app
-
-app = create_app()
 
 if __name__ == "__main__":
     app.run()
+
+
+# Want it working
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run()
